04ca2fdf7ecd3568ecb228dcc555af2e814f86a8
daily1092892 <daily1092892@culture.com>
2022-09-13 15:22:50 +0800

    update

---
+++ test0913/test0913/MYTest.swift
@@ -0,0 +1,33 @@
+//
+//  MYTest.swift
+//  test0913
+//
+//  Created by 田晓晨 on 2022/9/13.
+//
+
+import Foundation
+
+/// 方法1
+public func method1() -> Int {
+    return 1
+}
+
+/// 方法2
+fileprivate func method2() {
+    
+}
+
+/// 方法3
+private func method3() {
+    
+}
+
+/// 方法4
+internal func method4() {
+    
+}
+
+/// 方法5 default scope
+func method6() {
+    
+}
---
+++ test0913/test0913/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test0913
+//
+//  Created by 田晓晨 on 2022/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
